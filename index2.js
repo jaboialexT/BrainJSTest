db037c6ca4aeb34f067bc828c6f8e0cf49c67b40
jaboialexT <Fartypoopybutt@gmail.com>
2022-05-27 16:36:56 -0400

    training data consisting of greetings and farewells

---
+++ index2.js
@@ -0,0 +1,15 @@
+const brain = require('brain.js');
+const data = require('./data.json');
+
+const network = new brain.recurrent.LSTM();
+
+const trainingData = data.map(item => ({
+    input: item.text,
+    output: item.category
+}));
+
+network.train(trainingData);
+
+const output = network.run("farewell");
+
+console.log(`Category: ${output}`)
